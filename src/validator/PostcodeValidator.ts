c7ad5a5e6bb829db18e09ced92e3b1ee2642629c
Mark Knight <mark@markfknight.com>
2016-07-31 13:26:07 +0100

    feat(postcode): add US postcode validation

--- src/validator/PostcodeValidator.ts
+++ src/validator/PostcodeValidator.ts
@@ -1,11 +1,14 @@
 export enum PostcodeLocale {
-    GB
+    GB, US
 }
 
 export class PostcodeValidator {
     private static _postcodeLocale = {
         GB: (postcode: string): boolean => {
             return (/^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/.test(postcode));
+        },
+        US: (postcode: string): boolean => {
+            return (/^\d{5}(-\d{4})?$/.test(postcode));
         }
     }
 
